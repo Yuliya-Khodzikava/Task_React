a5aa644707efefb1391b082a4f149c6e5d38c340
Yuliya-Khodzikava <ykhodzikava@copyright.com>
2018-02-23 12:00:03 +0300

    Update of variables file

---
+++ src/resources/styles/variables.js
@@ -0,0 +1,11 @@
+const variables = {
+    white_bg_color: '#fff',
+    header_button_bg_color: '#e5e5e5',
+    header_button3_bg_color: '#4a90e2',
+    header_button_font_color: '#545454',
+    header_button_font_size: '15px',
+    header_button3_font_color: '#fff',
+    legendSizeTopPanel_bg_color: '#f3f3f3'
+}
+
+export default variables;
