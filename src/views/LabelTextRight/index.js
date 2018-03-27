b738ae53313a33332c5d51742c24cb1c32aabb7c
Yuliya-Khodzikava <ykhodzikava@copyright.com>
2018-03-27 15:57:31 +0300

    generation of values in attribute d in path tag

--- src/views/LabelTextRight/index.js
+++ src/views/LabelTextRight/index.js
@@ -8,7 +8,7 @@ class LabelTextRight extends Component {
                 <defs>
                     <path id="LabelTextRight" d="M526 320 C 536 335, 580 377, 526 425"/>
                 </defs>
-                <text  x="45" fill="#fbfbfb">
+                <text x="45" fill="#fbfbfb">
                     <textPath href="#LabelTextRight">
                         label
                     </textPath>
